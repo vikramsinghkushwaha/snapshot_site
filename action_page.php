cf081e8ecde223cbfa8ef79034ec69e4662c5598
Vikramsingh Kushwaha <34671021+vikramsinghkushwaha@users.noreply.github.com>
2018-06-20 19:42:55 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+  Welcome <?php echo $_GET["firstname"]; ?></br>
+  Your ph number is: <?php echo $_GET["mob_number"]; ?></br>
+  Your email address is: <?php echo $_GET["email"]; ?>
+
+</body>
+</html>
